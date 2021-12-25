8c6b660605d662854f1e1074af7cbbf5953476d6
Nathen Rader <nrader@live.com>
2021-12-25 01:38:41 +0000

    tsnode and setup

--- setupNode.sh
+++ setupNode.sh
@@ -5,4 +5,4 @@ export NVM_DIR="$HOME/.nvm" && (
   cd "$NVM_DIR"
   git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`
 ) && \. "$NVM_DIR/nvm.sh" \
-&& nvm install 17
+&& nvm install 17 && npm i;
